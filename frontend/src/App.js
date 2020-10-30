a7217d538d0164567edcc0a8989360113d56cbb6
NooraVino <noora.virolainen@yahoo.com>
2020-10-30 12:01:47 +0200

    queries

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,9 +3,33 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import { useQuery} from '@apollo/client'
+
+import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client'
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  })
+})
+
+const ALL_AUTHORS = gql`
+  query {
+    allAuthors  {
+      name,
+    }
+  }
+`
+
+
 
 const App = () => {
   const [page, setPage] = useState('authors')
+
+ 
+
+
 
   return (
     <div>
