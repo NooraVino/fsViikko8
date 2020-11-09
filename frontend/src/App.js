f0b10d538140a72a048eb0dd0dc76d8907e94b79
NooraVino <noora.virolainen@yahoo.com>
2020-11-09 15:31:14 +0200

    exercise 8.17

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,28 +3,74 @@ import React, { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import LoginForm from './components/LoginForm'
+import {useApolloClient } from '@apollo/client'
 
+const Notify = ({ errorMessage }) => {
+  if ( !errorMessage ) {
+    return null
+  }
+
+  return (
+    <div style={{color: 'red'}}>
+      {errorMessage}
+    </div>
+  )
+}
 
 
 const App = () => {
   const [page, setPage] = useState('authors')
-  
-  
+  const [token, setToken] = useState(null)
+  const [errorMessage, setErrorMessage] = useState(null)
+
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 5000)
+  }
+
+  if (!token) {
+    
+    return (
+      <div>
+        <Notify errorMessage={errorMessage} />
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('loginForm')}>kirjautumissivu</button>
+        <Authors show={page === 'authors'} />
+        <Books show={page === 'books'}/>
+        <LoginForm show={page === 'loginForm'} setToken={setToken}setError={notify} />
+      </div>
+    )
+  }
+
+
   return (
     <div>
 
       <div>
-        <button onClick={() => setPage('authors')}>authors</button>
-        <button onClick={() => setPage('books')}>books</button>
+      <button onClick={() => setPage('authorsWithB')}>authors</button>
+        <button onClick={logout} >logout</button>
+        <Authors show={page === 'authorsWithB'} token={token} />
+        <Notify errorMessage={errorMessage} />
+        <Books show={page === 'books'}/>
         <button onClick={() => setPage('add')}>add book</button>
       </div>
 
-      <Authors show={page === 'authors'} />
      
 
-      <Books
-        show={page === 'books'}
-      />
+
+      
 
       <NewBook
         show={page === 'add'}
--- frontend/src/components/Authors.js
+++ frontend/src/components/Authors.js
@@ -4,17 +4,53 @@ import {ALL_AUTHORS} from '../queries'
 import {useQuery } from '@apollo/client'
 import SetBirthday from './SetBirthday'
 
-const Authors = (props) => {
+const Authors = ({show, token}) => {
   const result = useQuery(ALL_AUTHORS)
- 
+
  if (result.loading) {
    return <div>loading...</div>
  }
  
-  if (!props.show) {
+  if (!show) {
     return null
   }
  const authors = result.data.allAuthors
+
+if (token) {
+  console.log(token)
+  return (
+    <div>
+      <SetBirthday authors={authors} />
+      <div>
+      <h2>authors</h2>
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              born
+            </th>
+            <th>
+              books
+            </th>
+          </tr>
+          {authors.map(a =>
+            <tr key={a.name}>
+              <td>{a.name}</td>
+              <td>{a.born}</td>
+              <td>{a.booksByAuthor}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+  
+    </div>
+
+      </div>
+    
+    
+  )
+}
 
   return (
     <div>
@@ -39,7 +75,7 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-      <SetBirthday authors={authors} />
+  
     </div>
   )
 }
